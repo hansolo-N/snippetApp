2810ef95a70a55a1b0de05d18e2437a49a9d48a3
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2024-01-31 10:19:03 +0200

    create snippet page updated

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,25 +1,16 @@
+"use client";
+
 import React from "react";
-import { db } from "@/db";
-import { redirect } from "next/navigation";
+import * as actions from "@/actions";
+import { useFormState } from "react-dom";
 
 function SnippetCreatePage() {
-  async function createSnippet(formData: FormData) {
-    "use server";
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
-
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    });
-    console.log(snippet);
-    redirect("/");
-  }
+  const [formState, action] = useFormState(actions.createSnippet, {
+    message: "",
+  });
 
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className="font-bold m-3"> create a snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -45,6 +36,13 @@ function SnippetCreatePage() {
             />
           </div>
         </div>
+
+        {formState.message ? (
+          <div className="my-2 p-2 bg-red-200 border rounded border-red-400">
+            {formState.message}
+          </div>
+        ) : null}
+
         <button type="submit" className="rounded p-2 bg-blue-200">
           create snippet
         </button>
